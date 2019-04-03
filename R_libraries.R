25f94ca8a7187b7fe587359e4b01cc063d58ba79
pplanb <rolandhablutzel@hotmail.com>
2019-04-03 18:35:24 +0200

    Update R_libraries.R

--- R_libraries.R
+++ R_libraries.R
@@ -1,8 +1,11 @@
+# make sure to verify and EDIT environmental variables
+# Control Panel >> User Accounts >> Change Environmental Variables >> R_LIBS_USER
+
 # set default library
 existing_library <- .libPaths()
 
 # define location of the libraries:
-new_library <- c("D:/R/R-3.5.1/library", "C:/Program Files/R/R-3.5.1/library")
+new_library <- c("D:/R/R-3.5.3/library", "C:/Program Files/R/R-3.5.3/library")
 
 # set location
 .libPaths(new_library)
