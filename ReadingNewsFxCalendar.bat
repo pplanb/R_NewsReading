50a763ce32b912e8530d5024f17acb91544d07ee
pplanb <rolandhablutzel@hotmail.com>
2019-04-10 22:51:12 +0200

    Update ReadingNewsFxCalendar.bat

--- ReadingNewsFxCalendar.bat
+++ ReadingNewsFxCalendar.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\R\R-3.5.1\bin\Rscript.exe" "D:\TradingRepos\R_NewsReading\ReadingNewsFxCalendar.R"
+"C:\Program Files\R\R-3.5.3\bin\Rscript.exe" "D:\TradingRepos\R_NewsReading\ReadingNewsFxCalendar.R"
